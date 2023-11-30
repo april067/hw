f949f5fa287d6667218f06c5c63327c0ca9d9471
april067 <ik.kiev.ua@gmail.com>
2023-11-30 21:57:19 +0200

    onBackdrop, onKeydown

--- js/modal.js
+++ js/modal.js
@@ -6,8 +6,39 @@ const refs = {
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.modal.addEventListener('click', onBackdrop);
+
+function removeKeydownListener() {
+	window.removeEventListener('keydown', onKeydown);
+}
 
 function toggleModal() {
-	refs.modal.classList.toggle('is-hidden');
 	document.body.classList.toggle('modal-open');
+	refs.modal.classList.toggle('is-hidden');
+	const isHidden = refs.modal.classList.contains('is-hidden');
+
+	if (!isHidden) {
+		window.addEventListener('keydown', onKeydown);
+		refs.openModalBtn.disabled = true; // WTF! This condition is required because refs.openModalBtn remains in focus when the refs.modal is open. And SPACE can open-close refs.modal until windowEventListener will be removed
+		return;
+	}
+
+	if (isHidden) {
+		removeKeydownListener();
+		refs.openModalBtn.disabled = false;
+		return;
+	}
+}
+
+function onBackdrop(e) {
+	if (e.target === e.currentTarget) {
+		toggleModal();
+		removeKeydownListener();
+	}
+}
+
+function onKeydown(e) {
+	if (e.code === 'Escape') {
+		toggleModal();
+	}
 }
